85b35d12aebb3e2719219384850a16ca9e0f3b10
Jumai Tijani <tijanij25@gmail.com>
2024-01-18 17:37:06 +0100

    add test file

--- main.py
+++ main.py
@@ -6,5 +6,3 @@ def addition(num1, num2):
 def subtraction(num1, num2):
     return num1 - num2
 
-
-    
--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 
+
 import main
 import unittest
 
 class TestMain(unittest.TestCase):
-    
+
     def test_addition(self):
-        self.assertEqual(main.adddition(1, 2), 3, "should be 3")
-        self.assertEqual(main.adddition(5, 5), 10, "should be 10")
-        self.assertEqual(main.adddition(-3, 2), -1, "should be -1")
+        self.assertEqual(main.addition(1, 2), 3, "Should be 3")
+        self.assertEqual(main.addition(5, 5), 10, "Should be 10")
+        self.assertEqual(main.addition(40, 20), 60, "Should be 60")
 
-        If __name__ == '__main__':
-        unittest.main()
+    def test_subtraction(self):
+        self.assertEqual(main.subtraction(10, 5), 5, "Should be 5")
 
-        
+
+if __name__ == '__main__ ':
+    unittest.main()   
+
