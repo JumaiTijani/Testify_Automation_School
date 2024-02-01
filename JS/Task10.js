bbd1694566f2964516700ac4518697e2991f2f6b
Jumai Tijani <tijanij25@gmail.com>
2024-02-01 17:37:13 +0100

    add task file

---
+++ JS/Task10.js
@@ -0,0 +1,18 @@
+//TASK 10/LESSON 24
+
+// Task: Area Of a Rectangle 2
+
+// Write JavaScript function, calculateAreaOfRectangle. The function should:
+
+// 1. Takes two parameters, length and width. 
+// 2. Multiplies the length and width
+// 3. Returns the area of the rectangle
+
+
+function calculateAreaOfRectangle(length, width) {
+    const product = length * width;
+    return [product];
+   }
+   
+   console.log(calculateAreaOfRectangle(4, 20));
+
---
+++ JS/Task12.js
@@ -0,0 +1,17 @@
+//TASK 12/LESSON 29
+
+// Task: Pick your Best Friend
+// Elements of an array are index based. 
+// From your array of best created in the previous task, choose your best friend based on their index in the array. 
+
+// Steps: 
+
+// 1. On the next line below your array of friends, Declare a variable, bestFriend and assign your best friend from the array of friends. 
+//(Use bracket notation)
+// 2. Log bestFriend to the console to ensure you chose the right friend.
+
+const friends = ['Adesumbo', 'Abimbola', 'Bolanle', 'Rahmat', 'Andem']
+
+const bestFriend = friends[3]
+
+console.log(bestFriend)
