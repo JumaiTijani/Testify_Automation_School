16cabf7d83ab7838f898c2b62226a74ba918a1e9
Jumai Tijani <tijanij25@gmail.com>
2024-02-09 08:35:09 +0100

    add task file

---
+++ JS/Task13.js
@@ -0,0 +1,20 @@
+// Task 13/LESSON 35
+// My Personal Library
+
+// Task
+// Using object literal, write a JavaScript object, books.
+
+// The book object should take the following properties
+// title (string)
+// description (string)
+// numberOfPages (number)
+// authour (string)
+// reading (boolean)
+
+const book = {
+    title:'The Wary Transgressor',
+    description:'A crime Fiction',
+    numberOfPages: 168,
+    author:'James Hadley Chase',
+    reading: false
+}
