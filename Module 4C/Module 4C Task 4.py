cd53a3c9cbe4fd70904153ad82c66d6c242ecd7e
Jumai Tijani <tijanij25@gmail.com>
2024-05-09 14:17:28 +0100

    Add New Task

---
+++ Module 4C/Module 4C Task 4.py
@@ -0,0 +1,19 @@
+"""
+Declare two numbers with any values
+Add or Subtract or Multiply the two numbers together
+Print out the value of the operation
+"""
+
+#Declare two numbers with any values
+numberA = 14
+numberB = 2
+
+#Add or Subtract or Multiply the two numbers together
+sum = numberA + numberB
+subtraction = numberA - numberB
+multiplication = numberA * numberB
+
+#Print out the value of the operation
+print("Sum of the numbers = ",sum)
+print("Subtraction c = ",subtraction)
+print("Multiplication of the numbers = ",multiplication)
